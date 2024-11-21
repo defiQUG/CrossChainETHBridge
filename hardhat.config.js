83a62fd7c1eeeedd712b59ee85eb881fe09621a1
Devin <devin@devin.ai>
2024-11-21 23:34:30 +0000

    fix: clean setup with @nomiclabs packages for ethers v5

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,6 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     version: "0.8.20",
