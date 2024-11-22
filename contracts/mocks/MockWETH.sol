b4af54749a7c365087d93d1e5a4435ec61bf190d
Devin <devin@devin.ai>
2024-11-22 05:39:32 +0000

    fix: update MockWETH contract implementation

--- contracts/mocks/MockWETH.sol
+++ contracts/mocks/MockWETH.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockWETH is ERC20 {
     constructor() ERC20("Wrapped Ether", "WETH") {}
 
-    function deposit() external payable {
+    function deposit() public payable {
         _mint(msg.sender, msg.value);
     }
 
